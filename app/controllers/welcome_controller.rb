f000c5ab06439010545b94991ef31ccfd145b285
jeffmusk <jeffprogramacion@gmail.com>
2017-11-06 17:52:01 -0500

    aregaldo welcom index muestra 1 acustica 5 electricas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+  	@acusticas = Product.all.where(subcategoria: 'acusticas') 
+  	@acustica = @acusticas.first
+  	 @products = Product.all.where(subcategoria: 'electricas') 
+  	@electricas = @products.take(5)
   end
 end
